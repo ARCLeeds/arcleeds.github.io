6c69ef199c16a3be0f39195802a24a0da57b6e5d
sparrow0hawk <alexjcoleman@gmail.com>
2020-06-24 14:46:29 +0100

    initial foray into making this full javascript function

--- assets/js/script-maker.js
+++ assets/js/script-maker.js
@@ -1,21 +1,79 @@
 /*
-* javascript for completing code section based on input fields
+ javascript for completing code section based on input fields
 */
 "use strict";
+/**
+ * Summary. 
+ * Description. 
+ * 
+ * @param  {} div
+ */
+var ARCJobScriptOMat = function(div) {
+    this.values = {};
+    this.containerDiv = div;
+    this.inputs = {};
+    this.inputs.features = {};
+    this.formrows = [];
+    this.settings = {
+        machine = [ ["arc3", "ARC3"], ["arc4", "ARC4"]],
+        defaults : {
+        email_address : "myemail@leeds.ac.uk",
+        }
+    };
+    return this;
+};
 
+/*
 
+extending ARCJobScriptOMat with prototype functions for building inputs and table
 
-const result = document.getElementById('job_mem');
+*/ 
 
-function inputHandler(e) {
+/** 
+ * Summary. create a new row in table
+ * Description. 
+ * 
+ * @param  {} rowid
+ * @param  {} left
+ * @param  {} right
+ */
+ARCJobScriptOMat.prototype.returnNewRow = function(rowid, left, right) {
+    // variables for left, right and table row
+    var l, r, tr
+    l = document.createElement("td");
+    r = document.createElement("td");
+    tr = document.createElement("tr");
+    l.id = rowid + "_left";
+    r.id = rowid + "_right";
+    tr.id = rowid;
+    l.innerHTML(left);
+    r.appendChild(right);
+    tr.appendChild(l);
+    tr.appendChild(r);
 
-  const memLine = '#$ -l h_vmem=';
+    return tr;
+};
 
-  result.innerHTML = memLine + e.target.value + 'G';
-}
-
-console.log(arcType)
-console.log(mem)
-console.log(result)
-
-mem.oninput = inputHandler
+/**
+ * Summary.
+ * Description. 
+ * @param  {} args
+ */
+ARCJobScriptOMat.prototype.newCheckBox = function(args) {
+    var tthis = this;
+    var newEl = document.createElement("input");
+    newEl.ype = "checkbox";
+    var formrows = this.formrows;
+    if(args.checked)
+        newEl.checked = true;
+    if(args.toggle)
+        newEl.onclick = newEl.onchange = function () {
+            formrows[args.toggle].style.display = newEl.checked ? "" : "none";
+            tthis.updateJobScript();
+        };
+    else
+        newEl.onclick = newEl.onchange = function () {
+            tthis.updateJobScript();
+        };
+    return newEl;
+};
